f4bc929b17992752b5d55eb53d9ceef668b20a91
Manish <ysmanish5@gmail.com>
2026-02-25 14:40:59 -0700

    Set control system intake gains to 0

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -58,10 +58,10 @@ public class Intake extends SubsystemBase {
     var slot0Configs = pivotMotorConfigs.Slot0;
     slot0Configs.kG = 0.0; // 
     slot0Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
-    slot0Configs.kV = 0.46000000834465027; // A velocity target of 1 rps results in 0.12 V output
+    slot0Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
     slot0Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
-    slot0Configs.kP = 20; // A positio n error of 2.5 rotations results in 12 V output
-    slot0Configs.kI = 0.00001; // no output for integrated error
+    slot0Configs.kP = 0.0; // A positio n error of 2.5 rotations results in 12 V output
+    slot0Configs.kI = 0.0; // no output for integrated error
     slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
     slot0Configs.GravityType = GravityTypeValue.Arm_Cosine;
     
