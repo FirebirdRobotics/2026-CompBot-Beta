c7f460d319b8c2c2e45933fad9e4d18c94537005
Manish <ysmanish5@gmail.com>
2026-02-27 22:38:07 -0700

    overite intake.java with the neat code I wrote for the advantagekit comp bot code a while back instead of the shotty edit i made more recently

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,46 +4,35 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 
-// import dev.doglog.DogLog;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
-  private final TalonFX m_pivotMotor = new TalonFX(IntakeConstants.pivotMotorCANID, "CANivore");
-  private final TalonFX m_rollerMotor = new TalonFX(IntakeConstants.rollerMotorCANID, "CANivore");
-
-  LEDs m_Leds;
+  private final TalonFX m_pivotMotor = new TalonFX(41, "CANivore");
+  private final TalonFX m_rollerMotor = new TalonFX(42, "CANivore");
 
   /** Creates a new Intake. */
-  public Intake(LEDs leds) {
-    m_Leds = leds;
-    
+  public Intake() {
     var pivotMotorConfigs = new TalonFXConfiguration();
-
-
-    pivotMotorConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    
+    pivotMotorConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     pivotMotorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
     pivotMotorConfigs.Feedback.SensorToMechanismRatio = 6.333;
-    pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 60;
-    pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 60;
+
+    pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 50;
+    pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 50;
+
 
     pivotMotorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
     pivotMotorConfigs.CurrentLimits.StatorCurrentLimit = 50;
@@ -51,37 +40,18 @@ public class Intake extends SubsystemBase {
     pivotMotorConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
     pivotMotorConfigs.CurrentLimits.SupplyCurrentLimit = 50;
 
-    
-
-
     // set slot 0 gains
     var slot0Configs = pivotMotorConfigs.Slot0;
-    slot0Configs.kG = 0.0; // 
-    slot0Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
+    slot0Configs.kG = 0.0; // Probably don't need kg
+    slot0Configs.kS = 0.18; // Add 0.25 V output to overcome static friction /*VALUE HAS BEEN TUNED  */
     slot0Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
     slot0Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
     slot0Configs.kP = 0.0; // A positio n error of 2.5 rotations results in 12 V output
     slot0Configs.kI = 0.0; // no output for integrated error
     slot0Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
-    slot0Configs.GravityType = GravityTypeValue.Elevator_Static;
-    
+    slot0Configs.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign;
+    slot0Configs.GravityType = GravityTypeValue.Elevator_Static; //FRC discord smart people say to use this
 
-    // var slot1Configs = pivotMotorConfigs.Slot1;
-    // slot1Configs.kG = 0.0; // 
-    // slot1Configs.kS = 0.0; // Add 0.25 V output to overcome static friction
-    // slot1Configs.kV = 0.0; // A velocity target of 1 rps results in 0.12 V output
-    // slot1Configs.kA = 0.0; // An acceleration of 1 rps/s requires 0.01 V output
-    // slot1Configs.kP = 0.0; // A positio n error of 2.5 rotations results in 12 V output
-    // slot1Configs.kI = 0.0; // no output for integrated error
-    // slot1Configs.kD = 0.0; // A velocity error of 1 rps results in 0.1 V output
-    // slot1Configs.GravityType = GravityTypeValue.Arm_Cosine;
-
-
-
-    // need to 
-    // var positionVoltageCongigs = pivotMotorConfigs.
-
-    // set Motion Magic settings
     var motionMagicConfigs = pivotMotorConfigs.MotionMagic;
     motionMagicConfigs.MotionMagicCruiseVelocity = 30.0; // Target cruise velocity of 80 rps
     motionMagicConfigs.MotionMagicAcceleration = 30.0; // Target acceleration of 160 rps/s (0.5 seconds)
@@ -98,14 +68,8 @@ public class Intake extends SubsystemBase {
 
     m_rollerMotor.getConfigurator().apply(intakeRollerConfigs);
 
-
-
-    
   }
 
-  // 0 for starting
-  // -0.243 for deployed
-  // Intake Deploys at inputed angle
   public void goToAngle(double angle) {
     // final PositionVoltage m_request = new PositionVoltage(angle).withSlot(0);
     final MotionMagicVoltage m_request = new MotionMagicVoltage(angle);
@@ -113,16 +77,13 @@ public class Intake extends SubsystemBase {
     m_pivotMotor.setControl(m_request.withPosition(angle));
   }
 
-  public Command goToDeployAndThenToUndeployCommand() {
+    public Command goToDeployAndThenToUndeployCommand() {
     return runEnd(
       () -> goToDeployedPosition(),
       () -> goToFramePerimeterPosition()
       
       ); 
   }
-
-
-  
 
   public void goToDeployedPosition() {
     // final PositionVoltage m_request = new PositionVoltage(IntakeConstants.deployAngle).withSlot(0);
@@ -137,7 +98,6 @@ public class Intake extends SubsystemBase {
 
   }
 
-
   public void goToFramePerimeterPosition() {
     final MotionMagicVoltage m_request = new MotionMagicVoltage(IntakeConstants.framePerimeterAngle);
 
@@ -146,17 +106,10 @@ public class Intake extends SubsystemBase {
     setRollerMotorPercentOutput(0);
   }
 
-
-
-  /* */
   public Command CommandGoToAngle(double angle) {
     return new InstantCommand(()-> goToAngle(angle), this);
   }
-  
 
-
-
-  
   public void setRollerMotorPercentOutput(double outputPercent) {
     m_rollerMotor.setControl(new DutyCycleOut(outputPercent));
   }
@@ -186,11 +139,12 @@ public class Intake extends SubsystemBase {
     
   }
 
+  
+
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-
-    
   }
 }
