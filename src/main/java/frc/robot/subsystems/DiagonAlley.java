ad6ebc1712368adf1373afe9190ccbbde024c07b
Manish <ysmanish5@gmail.com>
2026-03-03 16:18:50 -0700

    Base diag alley code

--- src/main/java/frc/robot/subsystems/DiagonAlley.java
+++ src/main/java/frc/robot/subsystems/DiagonAlley.java
@@ -4,11 +4,75 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.FloorRollerConstants;
 
 public class DiagonAlley extends SubsystemBase {
   /** Creates a new DiagonAlley. */
-  public DiagonAlley() {}
+
+    private final TalonFX m_leader = new TalonFX(FloorRollerConstants.floorRollerMotorCANID, "CANivore"); 
+    private final TalonFX m_follower = new TalonFX(FloorRollerConstants.floorRollerMotorCANID, "CANivore");
+
+  
+  public DiagonAlley() {
+    m_follower.setControl(new Follower(m_leader.getDeviceID(), MotorAlignmentValue.Opposed));
+
+    var rollerMotorConfigs = new TalonFXConfiguration();
+
+    rollerMotorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+  
+    rollerMotorConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    rollerMotorConfigs.CurrentLimits.SupplyCurrentLimit = 50;
+    rollerMotorConfigs.CurrentLimits.StatorCurrentLimit = 50;
+  
+    rollerMotorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    rollerMotorConfigs.CurrentLimits.StatorCurrentLimit = 50;
+  
+    rollerMotorConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    rollerMotorConfigs.CurrentLimits.SupplyCurrentLimit = 50;
+  
+    m_leader.getConfigurator().apply(rollerMotorConfigs);
+
+  }
+
+  public void setRollerMotorPercentOutput(double outputPercent) {
+    m_leader.setControl(new DutyCycleOut(outputPercent));
+  }
+  
+  public Command StartTurn(double power) {
+    return new InstantCommand(
+      () -> setRollerMotorPercentOutput(power)
+    );
+  }
+
+  public Command rollOutwards(double power) {
+    return new InstantCommand(
+      () -> setRollerMotorPercentOutput(power * -1)
+    );
+  }
+
+  public Command rollInwardsCommand(double power) {
+    return new InstantCommand(
+      () -> setRollerMotorPercentOutput(power)
+    );
+  }
+  
+  public Command Break(double power) {
+    return new InstantCommand(
+      () -> setRollerMotorPercentOutput(0)
+      );
+  }
+
 
   @Override
   public void periodic() {
