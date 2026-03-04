d22c3bd81fed1f5ee7eee0e4b52742bbfffe6de8
Manish <ysmanish5@gmail.com>
2026-03-03 20:03:21 -0700

    set intake pivot and roller motor CANIDs to correct values

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,8 +2,8 @@ package frc.robot.constants;
 
 public class IntakeConstants {
     // Need to set CAN IDs
-    public static final int pivotMotorCANID = 0;
-    public static final int rollerMotorCANID = 0;
+    public static final int pivotMotorCANID = 42;   // this is correct
+    public static final int rollerMotorCANID = 47;  // this is correct   
 
     public static final double deployAngle = -0.243;
     public static final double framePerimeterAngle = 0;
