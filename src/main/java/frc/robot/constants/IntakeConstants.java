216a53dcb2cd3f38fd976ce1dc7efaf7e4a81e01
Manish <ysmanish5@gmail.com>
2026-03-03 21:55:31 -0700

    made some variables

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -5,7 +5,8 @@ public class IntakeConstants {
     public static final int pivotMotorCANID = 42;   // this is correct
     public static final int rollerMotorCANID = 47;  // this is correct   
 
-    public static final double deployAngle = -0.243;
-    public static final double framePerimeterAngle = 0;
+    public static final double deployDistance = 0;
+    public static final double midPoint= 15.7;
+    public static final double framePerimeterDistance = 2; // True value is -0.1933 but this should be fine and will help protect mechanism in case of overshoot or shenanagins
 
 }
