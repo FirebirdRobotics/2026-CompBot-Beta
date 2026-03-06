ae5b3b82666c4ed3760914408354685bfcbffd87
Manish <ysmanish5@gmail.com>
2026-03-05 20:25:18 -0700

    working frameperimeterdistance and deploydistance constants for intake

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -5,8 +5,13 @@ public class IntakeConstants {
     public static final int pivotMotorCANID = 42;   // this is correct
     public static final int rollerMotorCANID = 47;  // this is correct   
 
-    public static final double deployDistance = 0;
+    // public static final double deployDistance = 22.4;
+    // REAL DEPLOY DISTANCE 4.59
+    public static final double deployDistance = 4.59;
+
     public static final double midPoint= 15.7;
-    public static final double framePerimeterDistance = 2; // True value is -0.1933 but this should be fine and will help protect mechanism in case of overshoot or shenanagins
+
+    // Real frame perimeter distance is 0.26
+    public static final double framePerimeterDistance = 1.3; // True value is -0.1933 but this should be fine and will help protect mechanism in case of overshoot or shenanagins
 
 }
